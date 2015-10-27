71df2d66873b47413d7058a4dce48e0e1dded191
Victor Loren <VictorLoren@users.noreply.github.com>
2015-10-26 20:46:30 -0700

    Add entertainment_center.py which holds list of movies

---
+++ entertainment_center.py
@@ -0,0 +1,24 @@
+from media import Movie 
+
+
+#Defining the movies
+movies = []
+
+#Finding Nemo
+movies.append( 
+	Movie(
+		'Finding Nemo',
+		'http://ia.media-imdb.com/images/M/MV5BMTY1MTg1NDAxOV5BMl5BanBnXkFtZTcwMjg1MDI5Nw@@._V1_SY317_CR0,0,214,317_AL_.jpg',
+		'https://youtu.be/SPHfeNgogVs',
+		5
+	)
+)
+#Ride Along
+movies.append( 
+	Movie(
+		'Ride Along',
+		'http://www.blackfilm.com/read/wp-content/uploads/2014/01/Ride-Along-Poster.jpg',
+		'https://www.youtube.com/watch?v=5klp6rkHIks',
+		4
+	)
+)
