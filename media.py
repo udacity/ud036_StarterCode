de15371548fa7a1ed5ab8daab50a2c5da1918b95
robin-yoo <robinyoo25@gmail.com>
2017-06-29 14:09:00 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,12 +1,15 @@
 import webbrowser
 
+# The movie blueprint
 class Movie():
+    # Create 4 properties or parameters for the movie
     def __init__(self, title, storyline, poster_image, trailer_youtube):
         self.title = title
         self.storyine = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # Open the youtube trailer in a browser
     def open_browser(self):
         webbrowser.open(self.trailer_youtube_url)
         
