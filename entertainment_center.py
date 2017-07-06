7b5bc391106250360455b204033649f39786e9e0
eliezer1993 <je_mejias@knights.ucf.edu>
2017-07-05 22:53:59 -0400

    interfaced with fresh_tomatoes.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,7 @@
 import media
+import fresh_tomatoes
 
 trainspotting_2 = media.movie("T2: Trainspotting","First there was an opportunity......then there was a betrayal.","https://upload.wikimedia.org/wikipedia/en/1/1c/T2_%E2%80%93_Trainspotting_poster.jpg","https://youtu.be/EsozpEE543w")
 
-trainspotting_2.show_trailer()
+movies = [trainspotting_2]
+fresh_tomatoes.open_movies_page(movies)
