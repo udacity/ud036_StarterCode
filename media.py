ba3858e73b755c258935f20935cc2badfa181b69
eliezer1993 <je_mejias@knights.ucf.edu>
2017-07-05 22:31:21 -0400

    added instance method for opening movie trailer utilizing webbrowser.open method module

--- media.py
+++ media.py
@@ -1,6 +1,12 @@
+import webbrowser
+
 class movie():
+    #initialize class movie which takes in: movie title, synopsis, url for poster image, and url for youtuev trailer
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
