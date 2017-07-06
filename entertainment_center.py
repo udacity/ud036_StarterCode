df3b7d409bafccf857a838fde92c753e060f0b59
eliezer1993 <je_mejias@knights.ucf.edu>
2017-07-05 22:32:07 -0400

    defined instance of class movie for movie Trainspotting 2

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,1 +1,5 @@
 import media
+
+trainspotting_2 = media.movie("T2: Trainspotting","First there was an opportunity......then there was a betrayal.","https://upload.wikimedia.org/wikipedia/en/1/1c/T2_%E2%80%93_Trainspotting_poster.jpg","https://youtu.be/EsozpEE543w")
+
+trainspotting_2.show_trailer()
