dfb46b9f8f542efd6ad5eed38a14946918e3a050
jye0325 <30542235+jye0325@users.noreply.github.com>
2017-07-29 17:28:02 -0400

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,47 +1,18 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
-#print(toy_story.storyline)
+terminator_2 = media.Movie("Terminator 2",
+                          "https://upload.wikimedia.org/wikipedia/en/8/85/Terminator2poster.jpg",
+                          "https://www.youtube.com/watch?v=VVZQ39i5G1s")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=-9ceBgWV8io")
-#print(avatar.storyline)
-#avatar.show_trailer()
+inception = media.Movie("Inception",
+                        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
+                        "https://www.youtube.com/watch?v=W7OTkEY1tMI")
 
-school_of_rock = media.Movie("School of Rock",
-                             "Storyline",
-                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
-
-ratatouille = media.Movie("Ratatouille",
-                          "Storyline",
-                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
-
-midnight_in_paris = media.Movie("Midnight in Paris",
-                                "Storyline",
-                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
-
-hunger_games = media.Movie("Hunger Games",
-                           "Storyline",
-                           "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
-"""
 batman = media.Movie("The Dark Knight",
-                     "Batman vs. Joker",
                      "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
                      "https://www.youtube.com/watch?v=_PZpmTj1Q8Q")
-batman.show_trailer()
-"""
 
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+
+movies = [terminator_2, inception, hunger_games, batman]
 fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-#print(media.Movie.__doc__)
