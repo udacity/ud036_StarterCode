528a1de1b7f2e129d614078ef7873de095c38de2
Divya Mathur <avidrutham@gmail.com>
2017-09-12 21:05:57 +0530

    resolve conflicts

--- media.py
+++ media.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import webbrowser
 
 class Movie():
@@ -16,22 +15,4 @@ class Movie():
     def show_trailer(self):
         webbrowser.open(self.trailer)
     
-=======
-import webbrowser
 
-class Movie():
-    """ This class provide a way to store movie related information """
-
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    
-    
-    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster = movie_poster
-        self.trailer = movie_trailer
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer)
-    
->>>>>>> origin/master
