28438720c1e773f7669c876ad6d1b740d1375280
Savi Singh <singh6@ELSOXFM-213683.science.regn.net>
2017-10-23 16:44:11 +0100

    Changed README file

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -122,7 +122,7 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+<div class="col-md-4 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
 </div>
