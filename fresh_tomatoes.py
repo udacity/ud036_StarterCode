8330e07cba5276b819d734f8f40fa420df0afd58
Savi Singh <singh6@ELSOXFM-213683.science.regn.net>
2017-10-23 13:00:18 +0100

    Changed name of variables in fresh_tomatoes.py to match project_media.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -135,16 +135,16 @@ def create_movie_tiles_content(movies):
     for movie in movies:
         # Extract the youtube ID from the url
         youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+            r'(?<=v=)[^&#]+', movie.youtube_trailer_url)
         youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+            r'(?<=be/)[^&#]+', movie.youtube_trailer_url)
         trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
                               else None)
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
+            poster_image_url=movie.poster_image,
             trailer_youtube_id=trailer_youtube_id
         )
     return content
