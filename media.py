183d08144f370b043e5b09add02a19bd403941fa
Rahul Vishwakarma (RVK) <rvk.ahul@gmail.com>
2017-10-24 23:53:38 +0530

    Added comments, documented classes and gave name to classes in media file

--- media.py
+++ media.py
@@ -1,6 +1,8 @@
-
+#Base class
 class Video(object):
+	'''Base Class contains all the variables and function that is going to be same for its derived class''' #documentation of class
 	VALID_RATINGS=["G", "PG", "PG-13", "R"]
+	__name__ = "Video"	#defalut variable to name the class
 
 	def __init__(self, title, duration, genre, poster_image, youtube_trailer):
 		self.title = title
@@ -8,27 +10,36 @@ class Video(object):
 		self.genre = poster_image
 		self.youtube_trailer = youtube_trailer
 
+#Child class inheriting Video
 class Movie(Video):
+	'''Movie class contains details of movie and inherits Video class'''
+	__name__ = "Movie"
 
 	def __init__(self, title, duration, genre, poster_image, youtube_trailer, storyline, director, producer):
-		Video.__init__(self, title, duration, genre, poster_image, youtube_trailer)
+		Video.__init__(self, title, duration, genre, poster_image, youtube_trailer) #Calling Base class constructor
 		self.storyline = storyline
 		self.director = director
 		self.producer = producer
 
+#Child class inheriting Video
 class MusicVideo(Video):
-	
+	'''MusicVideo class contains information of music video and inherits Video class'''
+	__name__ = "MusicVideo"
+
 	def __init__(self, title, duration, genre, poster_image, youtube_trailer, artist, director, lable, peak_billboard_position):
-		Video.__init__(self, title, duration, genre, poster_image, youtube_trailer)
+		Video.__init__(self, title, duration, genre, poster_image, youtube_trailer) #Calling Base class constructor
 		self.artist = artist
 		self.director = director
 		self.lable = lable
 		self.peak_billboard_position = peak_billboard_position
 
+#Child class inheriting Video
 class TVShows(Video):
+	'''TVShows class have information of TV Series and inherits Video class'''
+	__name__ = "TVShows"
 	
 	def __init__(self, title, duration, genre, poster_image, youtube_trailer, scenario, no_of_seasons, tv_station, created_by):
-		Video.__init__(self, title, duration, genre, poster_image, youtube_trailer)
+		Video.__init__(self, title, duration, genre, poster_image, youtube_trailer) #Calling Base class constructor
 		self.scenario = scenario
 		self.no_of_seasons = no_of_seasons
 		self.tv_station = tv_station
