7922ac64cf5bc333c397284bb63c7cab5b80a421
Rahul Vishwakarma (RVK) <rvk.ahul@gmail.com>
2017-10-25 10:55:30 +0530

    Added all the instances of Movie class, next to add instances of Music Video and Tv Shows.

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,17 +2,19 @@ import media
 import urllib
 import fresh_tomatoes
 
-boyka_undisputed=media.Movie("Boyka: Undisputed", "190 Minute", "Martial arts film", "https://images-na.ssl-images-amazon.com/images/M/MV5BNGQ4YjhmNzUtZTI1MC00OTQ3LTk4MTItYWJhNDdmNGZjOWU2XkEyXkFqcGdeQXVyMTA2NjI0MQ@@._V1_UY268_CR0,0,182,268_AL_.jpg","https://www.youtube.com/watch?v=KQtB3-NMfnU", "A fighter tries to save wife of another fighter who he accidently kill in a fight.", "Todor Chapkanov", "Boaz Davidson")
+#Movie class objects with movie details
 
-avatar=media.Movie("Avatar", "162 Minute", "Fantasy/Science fiction film", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY", "A Marine on an alien planet", "James Cameron", "James Cameron")
+boyka_undisputed=media.Movie("Boyka: Undisputed", "88 Minutes", "Martial Arts Film", "https://images-na.ssl-images-amazon.com/images/M/MV5BNGQ4YjhmNzUtZTI1MC00OTQ3LTk4MTItYWJhNDdmNGZjOWU2XkEyXkFqcGdeQXVyMTA2NjI0MQ@@._V1_UY268_CR0,0,182,268_AL_.jpg","https://www.youtube.com/watch?v=KQtB3-NMfnU", "A fighter tries to save wife of another fighter who he accidently kill in a fight.", "Todor Chapkanov", "Boaz Davidson")
 
-#interstellar=media.Movie("Interstellar", "A EX-Nasa pilot went to search and colonize life in other planet.", "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg", "https://www.youtube.com/watch?v=2LqzF5WauAw")
+avatar=media.Movie("Avatar", "161 Minutes", "Fantasy/Science Fiction Film", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY", "A Marine on an alien planet", "James Cameron", "James Cameron")
 
-#the_big_sick=media.Movie("The Big Sick", "Love Story of Pakistani comedian in American", "https://upload.wikimedia.org/wikipedia/en/6/69/The_Big_Sick.jpg", "https://www.youtube.com/watch?v=PJmpSMRQhhs")
+interstellar=media.Movie("Interstellar", "169 Minutes", "Epic Science Fiction Film", "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg", "https://www.youtube.com/watch?v=2LqzF5WauAw", "A EX-Nasa pilot went to search and colonize life in other planet.", "Christopher Nolan", "Emma Thomas")
 
-#inception=media.Movie("Inception", "Creating an idea in other person mind by the help of dream.", "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg", "https://www.youtube.com/watch?v=d3A3-zSOBT4")
+the_big_sick=media.Movie("The Big Sick", "117 Minutes", "Romantic Comedy Film ", "https://upload.wikimedia.org/wikipedia/en/6/69/The_Big_Sick.jpg", "https://www.youtube.com/watch?v=PJmpSMRQhhs", "Love Story of Pakistani comedian in American", "Michael Showalter", "Judd Apatow")
 
-#shutter_island= media.Movie("Shutter Island", "A us marshal stuck in an Asylum in an island.", "https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg", "https://www.youtube.com/watch?v=YDGldPitxic")
+inception=media.Movie("Inception", "148 Minutes", "Science Fiction Film", "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg", "https://www.youtube.com/watch?v=d3A3-zSOBT4", "Creating an idea in other person mind by the help of dream.", "Christopher Nolan", "Emma Thomas")
+
+shutter_island= media.Movie("Shutter Island", "138 Minutes", " Neo-Noir Psychological Thriller", "https://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg", "https://www.youtube.com/watch?v=YDGldPitxic", "A us marshal stuck in an Asylum in an island.", "Martin Scorsese", "Mike Medavoy")
 
 #the_accountant=media.Movie("The Accountant", "Story of autism guy as a clever accountant and army skill", "https://upload.wikimedia.org/wikipedia/en/thumb/e/e4/The_Accountant_%282016_film%29.png/220px-The_Accountant_%282016_film%29.png", "https://www.youtube.com/watch?v=DBfsgcswlYQ")
 
@@ -20,6 +22,6 @@ avatar=media.Movie("Avatar", "162 Minute", "Fantasy/Science fiction film", "http
 
 #the_sixth_sense=media.Movie("The Sixth Sense", "A child psychologist, starts treating a young boy who acts as a medium of communication between Crowe and a slew of unhappy spirits.", "https://upload.wikimedia.org/wikipedia/en/6/66/The_sixth_sense.jpg", "https://www.youtube.com/watch?v=VG9AGf66tXM")
 
-movies=[boyka_undisputed, avatar]#, interstellar, the_big_sick, inception, shutter_island, the_accountant, lucy, the_sixth_sense]
+movies=[boyka_undisputed, avatar, interstellar, the_big_sick, inception, shutter_island]#, the_accountant, lucy, the_sixth_sense]
 
 fresh_tomatoes.open_movies_page(movies)
