d2d74c5381f5fe7d486fba635a38b6eb667f609f
louisorluigi <powell.e.louis@gmail.com>
2017-10-26 20:24:26 +0100

    Delete media.py

--- media.py
+++
@@ -1,5 +0,0 @@
-class Movie():
-  def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
-      self.title = movie_title
-      self.poster_image_url = poster_image_url
-      self.trailer_youtube_url = trailer_youtube_url
