ed64a99a89c1af4c3a3df4a460a5e0f028906403
louisorluigi <powell.e.louis@gmail.com>
2017-10-27 15:11:55 +0100

    Add files via upload

--- project1_die_hard/media.py
+++ project1_die_hard/media.py
@@ -1,4 +1,7 @@
-# class to create a new movie entry with movie title, poster_image_url, and trailer_youtube_url #
+# class to create a new movie entry with movie title,
+# poster_image_url, and trailer_youtube_url
+
+
 class Movie():
     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
         """ This docstring explains the constructor method """
