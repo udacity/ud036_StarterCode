07ece85c440c151c4d922bf849fbdd7af2750bc1
rubanap <33280996+rubanap@users.noreply.github.com>
2017-11-21 17:19:12 +0530

    Update media.py

--- media.py
+++ media.py
@@ -2,12 +2,21 @@ import webbrowser
 
 
 class Movie():
+    """
+    Movie class to store the details of movie
+    """
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
+        """
+        Assign the movie instance variables with the given values
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster = poster_image
         self.trailer = trailer_youtube
 
     def show_trailor(self):
+        """
+        Open the trailer of the calling movie instance
+        """
         webbrowser.open(self.trailer)
