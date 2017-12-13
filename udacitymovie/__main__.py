c8877b0d9449c4bac6536b74af94b21876ce52bd
msb236 <malcolm.s.britton@gmail.com>
2017-12-12 19:14:10 -0500

    added module doc strings

--- udacitymovie/__main__.py
+++ udacitymovie/__main__.py
@@ -1,6 +1,7 @@
-import fresh_tomatoes
-import media
-from movie_listing import movie_listing
+"""create movies webpage from command line"""
+from . import fresh_tomatoes
+from . import media
+from . import movie_listing
 
 if __name__ == '__main__':
     movies = [media.Movie(**movie) for movie in movie_listing]
