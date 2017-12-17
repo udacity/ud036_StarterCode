a106913baacc3411c8e304e1ff93e8cdc48c7ed8
Puneet Jain <mailpuneet8264@gmail.com>
2017-12-17 13:13:14 +0530

    Removed local movie poster path and added link from the internet

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,7 +9,8 @@ from media import Movie
 interstellar = Movie("Interstellar", "Professor Brand (Michael Caine), \
                         a brilliant NASA physicist, is working on plans to save \
                         mankind by transporting Earth's population to a \
-                        new home via a wormhole.", "poster_images/interstellar.jpeg", \
+                        new home via a wormhole.", \
+                        "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg", \
                         "https://www.youtube.com/watch?v=Lm8p5rlrSkY")
 
 #bajirao_mastani is an object of class Movie and arguments passed are movie name, 
@@ -18,7 +19,8 @@ bajirao_mastani = Movie("Bajirao Mastani", "The heroic Peshwa Bajirao, \
                              married to Kashibai, falls in love with Mastani, \
                              a warrior princess in distress. They struggle to make \
                              their love triumph amid opposition from his conservative family.", \
-                             "poster_images/bajirao_mastani.jpg","https://www.youtube.com/watch?v=eHOc-4D7MjY")
+                             "https://upload.wikimedia.org/wikipedia/en/5/52/Bajirao_Mastani_Poster_2.jpg",\
+                             "https://www.youtube.com/watch?v=eHOc-4D7MjY")
 
 #titanic is an object of class Movie and arguments passed are movie name,
 #movie story line, poster image and youtube trailer link
@@ -29,7 +31,8 @@ titanic = Movie("Titanic", "'Titanic' is an epic, action-packed romance set \
                     liner of her era -- the 'ship of dreams' -- which ultimately \
                     carried over 1,500 people to their death in the ice cold waters \
                     of the North Atlantic in the early hours of April 15, 1912.", \
-                    "poster_images/titanic.jpeg","https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+                    "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",\
+                    "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
 
 #inception is an object of class Movie and arguments passed are movie name, 
 #movie story line, poster image and youtube trailer link
@@ -40,7 +43,8 @@ inception = Movie("Inception", "Dom Cobb (Leonardo DiCaprio) is a thief with the
                     Cobb gets a chance at redemption when he is offered a seemingly \
                     impossible task: Plant an idea in someone's mind. \
                     If he succeeds, it will be the perfect crime, but a dangerous \
-                    enemy anticipates Cobb's every move.", "poster_images/inception.jpeg",\
+                    enemy anticipates Cobb's every move.", \
+                    "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",\
                     "https://www.youtube.com/watch?v=8hP9D6kZseM")
 
 #orphan is an object of class Movie and arguments passed are movie name, 
@@ -51,7 +55,8 @@ orphan = Movie("Orphan", "Devastated by the loss of their unborn baby, Kate (Ver
                     named Esther, and soon the couple take their new daughter home. \
                     But when a dangerous series of events unfolds, Kate begins to suspect \
                     that there is something evil lurking behind the child's angelic exterior.",\
-                    "poster_images/orphan.jpeg","https://www.youtube.com/watch?v=WgxVIB2WuHU")
+                    "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Orphanposter.jpg/220px-Orphanposter.jpg",\
+                    "https://www.youtube.com/watch?v=WgxVIB2WuHU")
 
 #p_s_i_love_u is an object of class Movie and arguments passed are movie name, 
 #movie story line, poster image and youtube trailer link
@@ -61,7 +66,8 @@ p_s_i_love_u = Movie("P.S. I love U", "When Gerry (Gerard Butler), the husband o
                     Gerry plans ahead. Beginning on her 30th birthday, she receives \
                     the first in a series of letters written by him, designed to ease \
                     her grief and encourage her to move forward to a new life.", \
-                    "poster_images/psiloveu.jpeg","https://www.youtube.com/watch?v=CZzW6_hR068")
+                    "https://upload.wikimedia.org/wikipedia/en/7/7f/PS_I_Love_You_%28film%29.jpg",\
+                    "https://www.youtube.com/watch?v=CZzW6_hR068")
 
 def main():
     """Main function of the application"""
