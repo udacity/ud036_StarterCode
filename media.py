9efbd28e6f4ac98e36c996128a5c6444ac0a3e87
lightfootc <34262488+lightfootc@users.noreply.github.com>
2018-02-10 23:20:12 -0600

    Update media.py
    Changed the instances from being too long and change the variable in the instance method.

--- media.py
+++ media.py
@@ -14,7 +14,11 @@ class Movie():
     '''The variables associated with a specific instance is called instance
     variables. These are unique to an object. They can be access by using the
     self keyword inside the class and the instance name outside the class.'''
-    def __init__ (self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -23,4 +27,4 @@ class Movie():
     '''This function inside of the Movie class that has the first argument
     as self is called instance method.'''
     def show_trailer(self):
-        webbrowser.open(self.movie_trailer_url)
+        webbrowser.open(self.trailer_youtube_url)
