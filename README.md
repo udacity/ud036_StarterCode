21610a7b54ff3872a68eaf887bf99dca3593786c
Glitch (aviral-movies) <none>
2018-03-20 00:39:23 +0000

    🕶😨 Updated with Glitch

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Welcome to the Glitch BETA
-==========================
+=========================
 
 Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
 
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
+**Glitch** is the friendly commmunity where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
 
 Find out more [about Glitch](https://glitch.com/about).
 
--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const fs = require('fs');
 var movieArray = [];
 const movieImageFolder = __dirname + '/public/images/movies/';
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -19,18 +16,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-/*
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-*/
 
 fs.readdir(movieImageFolder, function (err, files) {
     files.forEach(function (movie) {
@@ -39,8 +24,6 @@ fs.readdir(movieImageFolder, function (err, files) {
             console.log(movie.slice(0,-4));
             movieArray.push(movie.slice(0,-4));
         }
-
-        //console.log(arr);
     });
 });
 
@@ -48,14 +31,7 @@ app.get("/movieList", function (request, response) {
     console.log(movieArray);
 	response.send(movieArray);
 });
-/*
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-*/
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
